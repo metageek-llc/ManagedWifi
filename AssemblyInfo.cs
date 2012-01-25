332492c249c9baa7b475adcae666b03f8164d281
CI on MB02 <no_email_setup@metageek.net>
2012-01-25 11:33:26 -0700

    merge issue in the automated process, resolving

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,9 +1,5 @@
 ﻿// Assembly ManagedWifi, Version 2010.6.21.0
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyVersion("2011.6.20.1365")]
-=======
 [assembly: System.Reflection.AssemblyVersion("2011.6.20.1366")]
->>>>>>> MergeBranch
 [assembly: System.Reflection.AssemblyConfiguration("")]
 [assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
 [assembly: System.Reflection.AssemblyTitle("Managed Wifi API")]
@@ -15,9 +11,5 @@
 [assembly: System.Reflection.AssemblyTrademark("")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.Runtime.InteropServices.Guid("e7646a9f-2d07-455a-b9ae-51eaf7281d2f")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyFileVersion("1.1.0.1365")]
-=======
 [assembly: System.Reflection.AssemblyFileVersion("1.1.0.1366")]
->>>>>>> MergeBranch
 [assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
